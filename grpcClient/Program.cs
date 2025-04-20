37ed3dd11e0fdaeea367939113afd907bcdc271a
dogustuluk <dogus.tuluk@gmail.com>
2025-04-20 15:12:37 +0300

    init
    init

---
+++ grpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿/* yüklenmesi gereken kütüphaneler
+ * Google.Protobuf : protobuf serialization ve deserialization işlemlerini yapan kütüphanedir.
+ * Grpc.Net.Client : .Net mimarisine uygun gRPC kütüphanesidir.
+ * Grpc.Tools      : Proto dosyalarını derlemek için gerekli compileri ve araçları içeren kütüphanedir.
+ */
+
+// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using grpcServer;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7051"); //-> bu adresteki grpc servisine bağlanır
+var greetClient = new Greeter.GreeterClient(channel); //-> Greeter olması gerektiğini proto dosyasındaki service adımızdan biliyoruz.
+HelloReply result = await greetClient.SayHelloAsync(new HelloRequest { Name = "Doğuş Tuluk" });
+Console.WriteLine(result.Message);
+
